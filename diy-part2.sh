867cc79855538af27a9f45aac94c72424a77ba12
oaoming <156073377+oaoming@users.noreply.github.com>
2026-02-01 10:22:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,14 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# =========================================================
+#  GL-MT300N-V2 32M Flash Mod - 32MB 扩容补丁
+# =========================================================
+
+# 1. 修改 DTS 分区表 (让内核识别 32MB 空间)
+sed -i 's/<0x50000 0xfb0000>/<0x50000 0x1fb0000>/g' target/linux/ramips/dts/mt7628an_glinet_gl-mt300n-v2.dts
+
+# 2. 修改 Image Makefile (解除编译生成的体积限制)
+sed -i '/mt7628an_glinet_gl-mt300n-v2/,/IMAGE_SIZE/s/IMAGE_SIZE := .*/IMAGE_SIZE := 32448k/' target/linux/ramips/image/mt76x8.mk
+
+# 3. (已删除修改IP的命令，默认保持 192.168.1.1)
